83332a80bac5827e910e741289202e3036ea1c3a
dimitris <d.psychogyios@gmail.com>
2016-03-03 16:44:08 +0200

    write a simple program for webcame preview in opencvtest

---
+++ opencvtest/Debug/src/main.d
@@ -0,0 +1,4 @@
+src/main.d: ../src/main.cpp \
+ /home/dimitris/opencv/opencvtest/include/main.hpp
+
+/home/dimitris/opencv/opencvtest/include/main.hpp:
--- opencvtest/src/main.cpp
+++ opencvtest/src/main.cpp
@@ -3,6 +3,14 @@
 
 int main ()
 {
-
+	cv::Mat frame;
+	cv::namedWindow("video",cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture camera(0);
+	while(1)
+	{
+		camera>>frame;
+		cv::imshow("video",frame);
+		if(cv::waitKey(10)>=0) break;
+	}
 	return 0;
 }
