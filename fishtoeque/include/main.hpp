089a7ca7b851962807d4055e8fdeb825f92820a6
dimitris <d.psychogyios@gmail.com>
2016-03-04 17:23:35 +0200

    contin remap formulas

--- fishtoeque/include/main.hpp
+++ fishtoeque/include/main.hpp
@@ -1,9 +1,18 @@
- #include "opencv2/highgui/highgui.hpp"
- #include "opencv2/imgproc/imgproc.hpp"
- #include <iostream>
- #include <stdio.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#define EQUE_HEIGHT 480
+#define EQUE_WIDTH	2*EQUE_HEIGHT
+#define FISHEYE_ANGLE 180
+
+
 
 
 /*computes the remapping matrices*/
 void computeEqueRemap(cv::Mat *mapX,cv::Mat *mapY);
+
+
 
--- fishtoeque/src/main.cpp
+++ fishtoeque/src/main.cpp
@@ -30,5 +30,30 @@ void computeEqueRemap(cv::Mat *mapX,cv::Mat *mapY)
 	 */
 
 	float planarTheta, planarRadius;
+	float sphericalTheta,sphericalPhi;
+	float sphericalX,sphericalY,sphericalZ;
+	float proTheta,proPhi;//projection angles
+	const float sphericalRadius=1;
+
+	int equeX,equeY;
+	//from equerectangular image pixels x,y to spherical cordinates.
+	//the radius of the sphere is ! for simplicity
+
+	sphericalTheta 	= -M_PI * (1-2*equeX  /  EQUE_WIDTH);
+	sphericalPhi	= M_PI_2 *(1-2*equeY  /  EQUE_HEIGHT );
+
+	// compute cartesian coordinates of a pixel in 3d sphere
+	sphericalX=cos(sphericalPhi)*sin(sphericalTheta);
+	sphericalY=cos(sphericalPhi)*cos(sphericalTheta);
+	sphericalZ=sin(sphericalPhi);
+
+	//compute the polar cordinates of 3d points to planar circular image
+
+	proTheta=atan2(sphericalZ,sphericalX);
+	proPhi=atan2(sqrt(pow(sphericalX,2.0)+pow(sphericalZ,2.0)),sphericalY);
+
+	planarTheta=proPhi/(FISHEYE_ANGLE/2.0);
+
+
 
 }
