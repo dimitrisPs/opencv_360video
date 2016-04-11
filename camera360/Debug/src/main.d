276a3fb5f2ff5f7211852fc8b99068f509abfa2d
Dimitrios Psyvhogyios <d.psychogyios@gmail.com>
2016-04-12 00:56:27 +0300

    Delete main.d

--- camera360/Debug/src/main.d
+++
@@ -1,7 +0,0 @@
-src/main.d: ../src/main.cpp \
- /home/dimitris/opencv/camera360/include/main.hpp \
- /home/dimitris/opencv/camera360/include/camera360.hpp
-
-/home/dimitris/opencv/camera360/include/main.hpp:
-
-/home/dimitris/opencv/camera360/include/camera360.hpp:
